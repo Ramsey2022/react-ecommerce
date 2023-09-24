c0cfae9119b6bcf0b4dcab6c9d4198c00bc757bf
Ramsey2022 <abovethelotus@yahoo.com>
2023-09-24 11:46:00 -0700

    setup router, structure, and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Navbar } from './components/navbar'
 
 function App() {
 
   return (
-    <div className='app'></div>
+    <div className='app'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' />
+          <Route path='/cart' />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
